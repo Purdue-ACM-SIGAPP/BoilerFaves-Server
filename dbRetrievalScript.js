e6585a323ffa8edbc57b9838ee88d47cc2f2f7da
Lucas Shanker <lshanke@purdue.edu>
2018-01-31 11:03:23 -0500

    Run store script daily

---
+++ dbRetrievalScript.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+var mysql = require('mysql')
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "boilerfaves"
+  });
+
+
+  app.get('/', (req, res) => 
+      {
+        const retrievalSQL = "SELECT * FROM foods ORDER BY name"
+        con.query(retrievalSQL, function (err, result) {
+            console.log(result);
+            res.send(result);
+            if (err) throw err;
+          });
+      });
+  
+  app.listen(3000, () => console.log('Server is listening on port 3000!'))
+  
